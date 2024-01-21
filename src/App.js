e4660ea35a6e33f548429a531fe95b62dd9c9568
aniket3765 <ingawale3765@gmail.com>
2024-01-21 15:32:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Components/Layout/Header";
+import React, { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <Header>Order your meal now!</Header>
+    </Fragment>
+  );
+}
+
+export default App;
